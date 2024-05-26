6db451a51ba4aceaf09b3387b256b9860a7aebfc
vishalmishra99 <132979738+vishalmishra99@users.noreply.github.com>
2024-05-27 01:20:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  server : {
+    proxy : {
+      '/api' : {
+        port : 3000,
+        target : 'https://carrerhub-backend.onrender.com' ,
+        changeOrigin : true,
+      }
+    }
+  }
 })
